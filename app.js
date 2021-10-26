e79d2f8ceccee87c9a5b494da28844c64c6cd3f5
SangYong-Jeong <mmaf@naver.com>
2021-10-26 13:31:57 +0900

    package.json

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /************* global require *************/
-require('dotenv').config();
+require('./modules/dotenv-init')();
 const express = require('express');
 const app = express();
 const path = require('path');
