747904c32478d0e6f59a9fcffb27b3a8f433e150
SangYong-Jeong <mmaf@naver.com>
2021-10-27 11:06:43 +0900

    admin-login design

--- routes/admin/auth-router.js
+++ routes/admin/auth-router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get(['/', '/login'], (req, res, next) => {
-  res.send('login');
+  res.render('admin/auth/login', { css: 'admin-login' });
 });
 
 module.exports = { name: '/auth', router };
