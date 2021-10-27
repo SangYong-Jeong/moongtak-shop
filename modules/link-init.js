1b1f4ec410bfea4662254114e082d2536e9ce7d7
SangYong-Jeong <mmaf@naver.com>
2021-10-27 13:23:47 +0900

    추가

--- modules/link-init.js
+++ modules/link-init.js
@@ -1,10 +1,10 @@
 module.exports = {
   admin: {
     index: [],
-    user: [{ path: '', method: 'GET', name: '회원 리스트' }],
+    user: [{ path: '', name: '회원 리스트' }],
     board: [
-      { path: 'init', method: 'GET', name: '게시판 관리' },
-      { path: '', method: 'GET', name: '게시물 관리' },
+      { path: 'init', name: '게시판 관리' },
+      { path: '', name: '게시물 관리' },
     ],
   },
 };
