fef35d157626340c2de2f25106308fb2fc505b17
SangYong-Jeong <mmaf@naver.com>
2021-10-27 15:46:36 +0900

    관리자 디자인 완료

--- routes/admin/board-router.js
+++ routes/admin/board-router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('/admin/board');
+  res.render('admin/board/board-list', { css: 'admin-board' });
 });
 
 router.post('/', (req, res, next) => {
