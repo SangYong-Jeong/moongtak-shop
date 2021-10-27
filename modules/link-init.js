588f54ef6a2f72195c5335ce45fa32cc9d0256c2
SangYong-Jeong <mmaf@naver.com>
2021-10-27 14:49:36 +0900

    link iteration

--- modules/link-init.js
+++ modules/link-init.js
@@ -1,6 +1,15 @@
 module.exports = {
   admin: {
     index: [],
+    main: [
+      { path: '/admin', path2: '/admin', src: 'b0' },
+      { path: '/admin/user', path2: '/admin/user', src: 'b1' },
+      { path: '/admin/prd', path2: '/admin/prd', src: 'b2' },
+      { path: '/admin/order', path2: '/admin/order', src: 'b3' },
+      { path: '/admin/board/init', path2: '/admin/board', src: 'b4' },
+      { path: '/admin/system', path2: '/admin/system', src: 'b6' },
+      { path: '//analytics.goole.com', path2: '/admin/analytics', src: 'b9' },
+    ],
     user: [{ path: '', name: '회원 리스트' }],
     board: [
       { path: 'init', name: '게시판 관리' },
