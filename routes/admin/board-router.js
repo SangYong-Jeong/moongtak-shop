c97d6e39714e9585f953932aa54ddba5b6c1e6ef
SangYong-Jeong <mmaf@naver.com>
2021-10-28 13:26:10 +0900

    lunch time

--- routes/admin/board-router.js
+++ routes/admin/board-router.js
@@ -2,8 +2,18 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
+router.get('/init', (req, res, next) => {
+  res.render('admin/board/board-init', { css: 'admin-board' });
+});
+
 router.get('/', (req, res, next) => {
-  res.render('admin/board/board-list', { css: 'admin-board' });
+  const boardType = req.query.type || 'default';
+  res.render('admin/board/board-list', { css: 'admin-board', boardType });
+});
+
+router.get('/:id', (req, res, next) => {
+  const boardType = req.query.type || 'default';
+  res.render('admin/board/board-view', { css: 'admin-board', boardType });
 });
 
 router.post('/', (req, res, next) => {
@@ -16,10 +26,6 @@ router.put('/', (req, res, next) => {
 
 router.delete('/', (req, res, next) => {
   res.send('/admin/board:DELETE');
-});
-
-router.get('/init', (req, res, next) => {
-  res.render('admin/board/board-init', { css: 'admin-board' });
 });
 
 router.post('/init', (req, res, next) => {
