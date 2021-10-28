4ab9987476951986fae2fc88aa45b5e364a1d448
SangYong-Jeong <mmaf@naver.com>
2021-10-28 15:32:42 +0900

    routes-index 수정

--- modules/link-init.js
+++ modules/link-init.js
@@ -3,11 +3,27 @@ module.exports = {
     index: [],
     main: [
       { path: '/admin', path2: '/admin', src: 'b0' },
-      { path: '/admin/user', path2: '/admin/user', src: 'b1' },
+      {
+        path: '/admin/user',
+        path2: '/admin/user',
+        src: 'b1',
+      },
       { path: '/admin/prd', path2: '/admin/prd', src: 'b2' },
-      { path: '/admin/order', path2: '/admin/order', src: 'b3' },
-      { path: '/admin/board/init', path2: '/admin/board', src: 'b4' },
-      { path: '/admin/system', path2: '/admin/system', src: 'b6' },
+      {
+        path: '/admin/order',
+        path2: '/admin/order',
+        src: 'b3',
+      },
+      {
+        path: '/admin/board/init',
+        path2: '/admin/board',
+        src: 'b4',
+      },
+      {
+        path: '/admin/system',
+        path2: '/admin/system',
+        src: 'b6',
+      },
       {
         path: '//analytics.google.com',
         path2: '/admin/analytics',
@@ -15,15 +31,15 @@ module.exports = {
         target: '_blank',
       },
     ],
-    user: [{ path: '', name: '회원 관리' }],
-    order: [{ path: '', name: '주문 관리' }],
+    user: [{ path: '', path2: '/admin/user', name: '회원 관리' }],
+    order: [{ path: '', path2: '/admin/order', name: '주문 관리' }],
     prd: [
-      { path: '', name: '상품 관리' },
-      { path: 'cate', name: '카테고리 관리' },
+      { path: '', path2: '/admin/prd', name: '상품 관리' },
+      { path: 'cate', path2: '/admin/prd/cate', name: '카테고리 관리' },
     ],
     board: [
-      { path: 'init', name: '게시판 관리' },
-      { path: '', name: '게시물 관리' },
+      { path: 'init', path2: '/admin/board/init', name: '게시판 관리' },
+      { path: '', path2: '/admin/board', name: '게시물 관리' },
     ],
   },
 };
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,48 @@
 /* 
 shop.moongtak.com/admin/
-[GET]     shop.moongtak.com/admin/auth/login
-[GET]     shop.moongtak.com/admin/auth/logout
-[GET]     shop.moongtak.com/admin/auth/join
+[GET]     shop.moongtak.com/admin/login                   - 로그인 화면
+[POST]    shop.moongtak.com/admin/login                   - 로그인 처리
+[GET]     shop.moongtak.com/admin/logout                  - 로그아웃 처리
 
-[GET]     shop.moongtak.com/admin/user/       - 회원리스트
-[GET]     shop.moongtak.com/admin/user/1      - 회원내용
-[PUT]     shop.moongtak.com/admin/user/1      - 회원내용수정
-[DELETE]  shop.moongtak.com/admin/user/1      - 회원삭제
+[GET]     shop.moongtak.com/admin/user?page=1             - 회원리스트
+[GET]     shop.moongtak.com/admin/user/1                  - 회원내용
+[GET]     shop.moongtak.com/admin/user/1?type=update      - 회원내용수정 화면
+[PUT]     shop.moongtak.com/admin/user                    - 회원내용수정 (저장)
+[DELETE]  shop.moongtak.com/admin/user                    - 회원삭제
 
-[GET]     shop.moongtak.com/admin/cate        - 카테고리 리스트
-[GET]     shop.moongtak.com/admin/cate/1      - 카테고리 내용
-[POST]    shop.moongtak.com/admin/cate        - 카테고리 생성
-[PUT]     shop.moongtak.com/admin/cate        - 카테고리 수정
-[DELETE]  shop.moongtak.com/admin/cate/1      - 카테고리 삭제
+[GET]     shop.moongtak.com/admin/cate                    - 카테고리 리스트
+[GET]     shop.moongtak.com/admin/cate/1                  - 카테고리 내용
+[GET]     shop.moongtak.com/admin/cate/1?type=update      - 카테고리 내용 수정 화면
+[POST]    shop.moongtak.com/admin/cate                    - 카테고리 생성
+[PUT]     shop.moongtak.com/admin/cate                    - 카테고리 수정
+[DELETE]  shop.moongtak.com/admin/cate                    - 카테고리 삭제
 
-[GET]     shop.moongtak.com/admin/prd         - 상품 리스트
-[GET]     shop.moongtak.com/admin/prd/1       - 상품 내용
-[POST]    shop.moongtak.com/admin/prd         - 상품 생성
-[PUT]     shop.moongtak.com/admin/prd         - 상품 수정
-[DELETE]  shop.moongtak.com/admin/prd/1       - 상품 삭제
+[GET]     shop.moongtak.com/admin/prd?page=1              - 상품 리스트
+[GET]     shop.moongtak.com/admin/prd/1                   - 상품 내용
+[GET]     shop.moongtak.com/admin/prd/1?type=update       - 상품 내용 수정 화면
+[POST]    shop.moongtak.com/admin/prd                     - 상품 생성
+[PUT]     shop.moongtak.com/admin/prd                     - 상품 수정
+[DELETE]  shop.moongtak.com/admin/prd                     - 상품 삭제
 
-[GET]     shop.moongtak.com/admin/order       - 주문 리스트
-[GET]     shop.moongtak.com/admin/order/1     - 주문 내용
-[PUT]     shop.moongtak.com/admin/order       - 주문 수정
-[DELETE]  shop.moongtak.com/admin/order/1     - 주문 삭제
+[GET]     shop.moongtak.com/admin/order?page=1            - 주문 리스트
+[GET]     shop.moongtak.com/admin/order/1                 - 주문 내용
+[GET]     shop.moongtak.com/admin/order/1?type=update     - 주문 내용 수정 화면
+[PUT]     shop.moongtak.com/admin/order                   - 주문 수정
+[DELETE]  shop.moongtak.com/admin/order                   - 주문 삭제
 
-- 게시판 생성자
-[GET]     shop.moongtak.com/admin/board/init  - 게시판 관리
-[GET]     shop.moongtak.com/admin/board       - 게시판 리스트
-[GET]     shop.moongtak.com/admin/board/1     - 게시판 내용
-[PUT]     shop.moongtak.com/admin/board       - 게시판 수정
-[DELETE]  shop.moongtak.com/admin/board/1     - 게시판 삭제
+- 게시판 관리
+[GET]     shop.moongtak.com/admin/binit?page=1            - 게시판 관리 리스트/생성 화면
+[POST]    shop.moongtak.com/admin/binit                   - 게시판 생성
+[PUT]     shop.moongtak.com/admin/binit                   - 게시판 수정
+[DELETE]  shop.moongtak.com/admin/binit                   - 게시판 삭제
+
+- 게시물 관리
+[GET]     shop.moongtak.com/admin/board?page=1            - 게시판 리스트
+[GET]     shop.moongtak.com/admin/board/1                 - 게시판 내용
+[GET]     shop.moongtak.com/admin/board/1?type=update     - 게시판 수정 화면
+[POST]    shop.moongtak.com/admin/board                   - 게시판 수정
+[PUT]     shop.moongtak.com/admin/board                   - 게시판 수정
+[DELETE]  shop.moongtak.com/admin/board/1                 - 게시판 삭제
 
 shop.moongtak.com/api/
 
