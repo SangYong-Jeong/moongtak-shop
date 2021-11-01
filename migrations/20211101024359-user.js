324d3cc21f5afbb6903137671a62fc50eb92d59b
SangYong-Jeong <mmaf@naver.com>
2021-11-01 12:20:48 +0900

    migration에 대해 배움/ create board table

---
+++ migrations/20211101024359-user.js
@@ -0,0 +1,22 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('user', 'addrPost', {
+      type: Sequelize.CHAR(5),
+    });
+    await queryInterface.addColumn('user', 'addrRoad', {
+      type: Sequelize.STRING(255),
+    });
+    await queryInterface.addColumn('user', 'addrJibun', {
+      type: Sequelize.STRING(255),
+    });
+    await queryInterface.addColumn('user', 'addrComment', {
+      type: Sequelize.STRING(255),
+    });
+    await queryInterface.addColumn('user', 'addrDetail', {
+      type: Sequelize.STRING(255),
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    // await queryInterface.dropTable('users');
+  },
+};
