68f9740dda38525f5a62f27e6be2ac895e82af2a
SangYong-Jeong <mmaf@naver.com>
2021-11-03 13:18:34 +0900

    4교시 완료!

--- public/js/binit/binit.js
+++ public/js/binit/binit.js
@@ -1,5 +1,34 @@
-$('form[name="binitForm"]').submit(onInitCreate);
-function onInitCreate() {}
+$('form[name="binitForm"]').submit(onInitBoard);
+$('.binit-update-form').submit(onInitBoard);
 
-$('.binit-update-form').submit(onInitUpdate);
-function onInitUpdate() {}
+function onInitBoard(e) {
+  e.preventDefault();
+  var f = this;
+  var el = f.elements;
+  var title = f.title;
+  var titleValue = title.value.trim();
+  if (titleValue === '') {
+    alert('제목은 필수사항 입니다.');
+    title.focus();
+    return false;
+  }
+  f.submit();
+}
+
+function onChangeBoard(_method, el) {
+  el.form._method.value = _method;
+  el.form.submit();
+}
+
+/* var validation = true;
+for (var i = 0; i < el.length; i++) {
+  if ($(el[i]).props('required')) {
+    if (el[i].value.trim() === '') {
+      alert(el[i].dataset['title'] + ' 필수사항 입니다.');
+      el[i].focus();
+      validation = false;
+      break;
+    }
+  }
+}
+if (validation) f.submit(); */
