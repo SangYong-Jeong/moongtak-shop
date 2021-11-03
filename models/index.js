d0b044b24e277e8b87e0340cf91cabe95ff0d554
SangYong-Jeong <mmaf@naver.com>
2021-11-03 11:33:36 +0900

    회원관리 완료!

--- models/index.js
+++ models/index.js
@@ -5,13 +5,13 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
 config.timezone = '+09:00';
 const db = {};
-
 const sequelize = new Sequelize(
   config.database,
   config.username,
   config.password,
   config
 );
+
 fs.readdirSync(__dirname)
   .filter((file) => file !== 'index.js')
   .forEach((file) => {
