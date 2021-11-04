d556656bd7cfb6eb550c794605589ed01cecc701
SangYong-Jeong <mmaf@naver.com>
2021-11-04 10:26:18 +0900

    board-form 작업중

---
+++ public/js/board/default-form.js
@@ -0,0 +1,29 @@
+const { alert } = require('../../../modules/util');
+
+$('form[name="defaultForm"]').submit(onFormSubmit);
+function onFormSubmit(e) {
+  e.preventDefault();
+  var f = this;
+  var titleEl = f.title;
+  var writerEl = f.writer;
+  var contentEl = f.content;
+  var title = titleEl.value.trim();
+  var writer = writer.value.trim();
+  var content = content.value.trim();
+  if (title === '') {
+    alert('제목은 필수사항 입니다.');
+    titleEl.focus();
+    return false;
+  }
+  if (writer === '') {
+    alert('작성자는 필수사항 입니다.');
+    writerEl.focus();
+    return false;
+  }
+  if (content === '') {
+    alert('내용은 필수사항 입니다.');
+    contentEl.focus();
+    return false;
+  }
+  f.submit();
+}
