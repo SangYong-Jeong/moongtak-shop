72fae92ea9dc28394c57ce2c812c8d0ef30a9bec
SangYong-Jeong <mmaf@naver.com>
2021-11-12 10:54:45 +0900

    cate seeder 완료

---
+++ 02.cate.js
@@ -0,0 +1,9 @@
+const fs = require('fs-extra');
+const path = require('path');
+const { findChildId } = require('./modules/util');
+
+const [jsonFile] = fs.readJsonSync(path.join(__dirname, './json/tree.json'));
+const cateIds = findChildId(jsonFile, []);
+
+console.log(jsonFile);
+console.log(cateIds);
